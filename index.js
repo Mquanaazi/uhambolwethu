7befd3dce9f3ba1da141c7c9b4b62acc15eaa1e2
mquanaazi <olwethumalumla31@gmail.com>
2024-09-13 13:33:04 +0200

    :bug:

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import checkoutRoutes from './routes/checkoutRoutes.js'
 
 
 const app=express()
-app.use(cors({ origin: ['*','http://localhost:8080'], credentials: true }))
+app.use(cors({ origin: ['uhambolwettu.web.app','http://localhost:8080'], credentials: true }))
 const frontendPort = process.env.FRONTEND_PORT || 8081
 // app.use(cors({ origin: `http://localhost:${frontendPort}`, credentials: true }))
 
