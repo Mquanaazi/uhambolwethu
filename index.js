86affb6d53e0110c09179e272d00a804064b4981
mquanaazi <olwethumalumla31@gmail.com>
2024-09-13 14:12:01 +0200

    :pencil:

--- index.js
+++ index.js
@@ -6,22 +6,20 @@ import checkoutRoutes from './routes/checkoutRoutes.js'
 
 
 
-const app=express()
-app.use(cors({ origin: ['https://uhambolwettu.web.app','http://localhost:8080', 'uhambolwettu.firebaseapp.com'], credentials: true }))
 // const frontendPort = process.env.FRONTEND_PORT || 8081
 // app.use(cors({ origin: `http://localhost:${frontendPort}`, credentials: true }))
-
-let port=process.env.PORT
 // app.use(cors({origin:'http://localhost:8080',credentials:true}))
+
+const app=express()
+let port=process.env.PORT || 2027
+app.use(cors({ origin: ['https://uhambolwettu.web.app','http://localhost:8080', 'uhambolwettu.firebaseapp.com'], credentials: true }))
 app.use(express.json())
-
-
-
+app.use(express.static('public'))
 
 app.use('/assets',assetsRoutes)
 app.use( '/users',usersRoutes)
 app.use( '/checkOut',checkoutRoutes)
-app.use(express.static('static'))
+
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`)
     
